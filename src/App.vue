70ca937bd91c9e48ce76499c0fc1f9ede5a503bc
fangxinggguang <464229614@qq.com>
2016-11-28 00:14:45 +0800

    Signed-off-by: fangxinggguang <464229614@qq.com>

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <my-header></my-header>
+      <div class="main">
+        <keep-alive>
+          <router-view></router-view>
+        </keep-alive>
+      </div>
+    <my-footer></my-footer>
+  </div>
+</template>
+
+<script>
+import MyHeader from './components/header'
+import MyFooter from './components/footer'
+
+export default {
+  name: 'app',
+  components: {
+    MyHeader, MyFooter
+  }
+}
+</script>
+
+<style>
+body{
+  margin:0;
+  padding:0;
+}
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+.main{
+  height:500px;
+}
+</style>
---
+++ src/views/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    index view
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
